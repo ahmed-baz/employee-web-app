403ab2184d266b09e65af37d1efbb47351c2db3f
Ahmed Baz <ahmedbaz1024@gmail.com>
2024-12-09 20:00:09 +0200

    update employee

--- src/app/employee/model/employee.ts
+++ src/app/employee/model/employee.ts
@@ -1,44 +1,10 @@
 export class Employee {
 
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  email!: string;
+  salary?: number;
+  joinDate?: Date;
 
-  constructor(id: number, firstName: string, lastName: string, email: string, salary: number, joinDate: Date) {
-    this._id = id;
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._email = email;
-    this._salary = salary;
-    this._joinDate = joinDate;
-  }
-
-  private _id?: number;
-  private _firstName?: string;
-  private _lastName?: string;
-  private _email?: string;
-  private _salary?: number;
-  private _joinDate?: Date;
-
-
-  get id(): number | undefined {
-    return this._id;
-  }
-
-  get firstName(): string | undefined {
-    return this._firstName;
-  }
-
-  get lastName(): string | undefined {
-    return this._lastName;
-  }
-
-  get email(): string | undefined {
-    return this._email;
-  }
-
-  get salary(): number | undefined {
-    return this._salary;
-  }
-
-  get joinDate(): Date | undefined {
-    return this._joinDate;
-  }
 }
